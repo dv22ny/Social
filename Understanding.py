7c01e777adf5830d41ae867a9db00ddda3aadb36
dv22ny <73257081+dv22ny@users.noreply.github.com>
2020-12-08 11:23:12 -0500

    Helps us figure chizz out

--- Understanding.py
+++ Understanding.py
@@ -4,19 +4,19 @@ name = str(input("What is your name? "))
 print("Hello, " + name)
 time.sleep(1)
 print("Nice to meet you")
-age = int(input("How old are you?"))
+age = int(input("How old are you?" ))
 if age < 13:
     print("Unfortunately, you're too young for this. Thank you!")
     time.sleep(1)
     sys.exit
-elif age >= 13 and age <= 15:
+elif age >= 13 or age <= 15:
     oui = str(input("This may contain sensitive material. Would you like to continue? "))
-    if oui == 'Yes' or 'yes':
-        print("You have chose to continue, nice!")
+    if oui == 'Yes' or oui == 'yes' :
+        print("You have chosen to continue, nice!")
     else:
         print("You have chosen to leave, totally ok, tata!")
         time.sleep(1)
-        sys.exit
+        sys.exit()
 else:
     print("Yeah buddy!")
 time.sleep(1)
@@ -27,10 +27,16 @@ time.sleep(1)
 print("You're going to be asked a series of questions, which will help you determine what kind of girl is ideal for you OwO")
 time.sleep(2)
 buddy_cool = str(input("Buddy are you cool with that?"))
-if buddy_cool == 'Yes' or 'yes':
+if buddy_cool == 'Yes' or  buddy_cool == 'yes' :
     print("Ok bet let's start")
     time.sleep(2)
 else:
     print("OK that's fine you suck but bye")
     time.sleep(1)
-    sys.exit
+    sys.exit()
+q1 = int(input("How important do danyou think a girl's beauty is? 1 for lowest, 10 for highest. "))
+time.sleep(1)
+print("Muchas Gracias")
+q2 = int(input("How important do you think a girl's body is? 1 for lowest, 10 for highest. "))
+time.sleep(1)
+print("Again, Muchas Gracias")
